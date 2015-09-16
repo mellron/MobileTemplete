24e4e9da4afa06d98193fe4ea43410d02c25adad
detolle <detolle@WN-OH149EY7F524.us.bank-dns.com>
2015-09-16 15:11:54 -0400

    Templete version 1.0

--- public_html/scripts/main.js
+++ public_html/scripts/main.js
@@ -1,1 +1,38 @@
+$(document).ready(onReadyEvents);
+
  
+/******************************************************
+     Name: onReadyEvents
+     Paramters: 
+     Returns: N/A
+
+     Description:  Will fire all on ready events when page is complete
+                   Loaded.
+
+    *******************************************************/
+  function onReadyEvents()
+  {
+      //Note found that some browsers call this 
+      //if any part of the DOM is refreshed.                 
+            
+      InitEvents();
+                  
+      return true;
+  }
+  
+ /******************************************************
+     Name: InitEvents
+     Paramters: 
+     Returns: N/A
+
+     Description:  Used to Initialize varables and functinos
+
+  *******************************************************/
+  function InitEvents()
+  {     
+     
+    
+     
+    
+  }
+   
